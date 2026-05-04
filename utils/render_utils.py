b80fb1be4fcf4befa859babca92c0086a63ec3c6
Eric-04 <ekcho@vscode1.oscar.ccv.brown.edu>
2026-05-04 15:08:05 -0400

    fix: singular matrix error

--- utils/render_utils.py
+++ utils/render_utils.py
@@ -71,7 +71,7 @@ def focus_point_fn(poses: np.ndarray) -> np.ndarray:
   directions, origins = poses[:, :3, 2:3], poses[:, :3, 3:4]
   m = np.eye(3) - directions * np.transpose(directions, [0, 2, 1])
   mt_m = np.transpose(m, [0, 2, 1]) @ m
-  focus_pt = np.linalg.inv(mt_m.mean(0)) @ (mt_m @ origins).mean(0)[:, 0]
+  focus_pt = np.linalg.pinv(mt_m.mean(0)) @ (mt_m @ origins).mean(0)[:, 0]
   return focus_pt
 
 def transform_poses_pca(poses: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
